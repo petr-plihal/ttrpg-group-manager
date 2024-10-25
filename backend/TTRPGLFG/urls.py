4afda8e4328586c4b252b1462ba525991f72e9da
saytaM12 <matyas.oujezdsky@gmail.com>
2024-10-26 00:11:20 +0200

    added views to the app

--- backend/TTRPGLFG/urls.py
+++ backend/TTRPGLFG/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("user/<int:user_id>/", views.user_detail, name="user_detail"),
+    path("group/<int:group_id>/", views.group_detail, name="group_detail"),
+    path("user/<int:user_id>/groups/", views.user_groups, name="user_groups"),
+    path("group/<int:group_id>/sessions/", views.group_sessions, name="group_sessions"),
 ]
