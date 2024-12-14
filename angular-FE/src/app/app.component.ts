e3db759784771d36e5cd08b337b8d8195e00d90c
n123443 <n123443@gmail.com>
2024-12-14 14:14:47 +0100

    Sidenav styling

--- angular-FE/src/app/app.component.ts
+++ angular-FE/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RouterModule],
+  imports: [RouterOutlet, LoginComponent, RouterModule, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
